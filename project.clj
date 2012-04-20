96044f5a42e9014beea1b6812c761f3b1926dbcc
Bryan Weber <bweber@arin.net>
2012-04-20 15:09:45 -0400

    adding gh-pages branch with marginalia documentation

--- project.clj
+++
@@ -1,5 +0,0 @@
-(defproject com.brweber2/clj-dns "0.0.1-SNAPSHOT"
-  :description "Clojure wrapper library for [dnsjava](http://www.xbill.org/dnsjava). Project at [clj-dns on github](http://github.com/brweber2/clj-dns)"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [dnsjava/dnsjava "2.1.1"]]
-  :dev-dependencies [[lein-marginalia "0.7.0"]])
--- test/clj_dns/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns clj-dns.test.core
-  (:use [clj-dns.core])
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
